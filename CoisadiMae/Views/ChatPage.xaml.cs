fa619a85f8c680d22ba49ba51a15c1e24166893c
Rodrigo Amaro <jbravo.br@gmail.com>
2017-05-28 13:27:30 -0300

    Äjustes de layout iOS

--- CoisadiMae/Views/ChatPage.xaml.cs
+++ CoisadiMae/Views/ChatPage.xaml.cs
@@ -8,41 +8,11 @@ namespace CoisadiMae.Views
         public ChatPage()
         {
             InitializeComponent();
-            //lvtChat.BindingContextChanged += LvtChat_BindingContextChanged;
-            lvtChat.PropertyChanged += LvtChat_PropertyChanged;
-            lvtChat.DescendantAdded += LvtChat_DescendantAdded;
+            lvtChat.ItemSelected+= (sender, e) => {
+                ((ListView)sender).SelectedItem = null;
+            };
         }
 
-        private void LvtChat_DescendantAdded(object sender, ElementEventArgs e)
-        {
-            lvtChat.ScrollTo(e.Element, ScrollToPosition.End, true);
-        }
-
-        private void LvtChat_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-
-            if (lvtChat.ItemsSource != null)
-            {
-                object lastItem = null;
-                foreach (var item in lvtChat.ItemsSource)
-                {
-                    lastItem = item;
-                }
-
-                lvtChat.ScrollTo(lastItem, ScrollToPosition.End, true);
-            }
-        }
-
-        private void LvtChat_BindingContextChanged(object sender, System.EventArgs e)
-        {
-            //var x = Enumerable.Cast<Lis>
-            object lastItem = null;
-            foreach (var item in lvtChat.ItemsSource)
-            {
-                lastItem = item;
-            }
-
-            lvtChat.ScrollTo(lastItem, ScrollToPosition.End, true);
-        }
+       
     }
 }
